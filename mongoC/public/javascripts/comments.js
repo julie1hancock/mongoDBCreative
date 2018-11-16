a9e6f44f7ff808249cf12681784b625be21f068f
EC2 Default User <ec2-user@ip-172-31-14-158.us-east-2.compute.internal>
2018-11-16 07:31:01 +0000

    i think done?

--- mongoC/public/javascripts/comments.js
+++ mongoC/public/javascripts/comments.js
@@ -1,5 +1,125 @@
 $(document).ready(function(){
-  $("#postComment").click(function(){
+
+  var commentList = "COMMENTS:<ul>";
+  function comment(){
+    var c = "<li>" + $("#name").val() + ": \"" + $("#comment").val() + "\"" + "</li>";
+    commentList+= c;
+    $("#comments").html(commentList + "</ul>");
+  }
+
+  $("#register").click(function(){
+      var myobj = {Name:$("#name").val(),Password:$("#password").val()};
+      var jobj = JSON.stringify(myobj);
+      var url = "register";
+        $.ajax({
+        url:url,
+        type: "POST",
+        data: jobj,
+        contentType: "application/json; charset=utf-8",
+        success: function(data,textStatus) {
+          if (data === "username taken")
+            $("#success").text("sorry user already registered");
+          else if (data === "registering user"){
+            $("#success").text("successfully registered!");
+            comment();
+          }
+          else if (data === "empty"){// || $("#comment").val === ""){
+            $("#success").text("please don't leave any space empty");
+          }
+        }
+        })//end ajax
+  });//end #register
+
+
+  $("#login").click(function(){
+      var myobj = {Name:$("#name").val(),Password:$("#password").val()};
+      var jobj = JSON.stringify(myobj);
+      var url = "login";
+        $.ajax({
+        url:url,
+        type: "POST",
+        data: jobj,
+        contentType: "application/json; charset=utf-8",
+        success: function(data,textStatus) {
+          if (data === "logged in"){
+            $("#success").text("successfully logged in!");
+            comment();
+          }
+          else if (data === "username doesn't exist"){
+            $("#success").text("entered username doesn't match any in database");
+          }
+          else if (data === "empty"){// || $("#comment").val === ""){
+            $("#success").text("please don't leave any space empty");
+          }
+        }
+        })//end ajax
+  });//end #register
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ $("#users").click(function() {
+  /*    var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+      var jobj = JSON.stringify(myobj);
+      $("#json").text(jobj);
+    */  
+      var url = "users";
+        $.ajax({
+        url:url,
+        type: "GET",
+//        data
+          contentType: "application/json; charset=utf-8",
+          success: function(data,textStatus) {
+            var every = "USERS:<ul>";
+            for(var i = 0; i < data.length; i++){
+            //  console.log("NAME:" + data[0].Name);
+              every = every + "<li>" + data[i].Name + "</li>";
+            }
+            every += "</ul>";
+            $("#success").html(every);
+          }
+        })
+})
+
+
+
+
+
+
+ $("#clear").click(function() {
+      var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+      var jobj = JSON.stringify(myobj);
+          $("#json").text(jobj);
+      
+      var url = "delete";
+        $.ajax({
+        url:url,
+        type: "POST",
+        data: jobj,
+        contentType: "application/json; charset=utf-8",
+        success: function(data,textStatus) {
+        }
+        })
+       $("#comments").html("COMMENTS:");
+ 
+   
+ })
+
+
+
+
+/*  $("#postComment").click(function(){
       var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
       jobj = JSON.stringify(myobj);
       $("#json").text(jobj);
@@ -57,5 +177,5 @@ $(document).ready(function(){
         $("#json").text("");
         $("#comments").text("");
     })
-
+*/
 });
--- mongoC/routes/index.js
+++ mongoC/routes/index.js
@@ -7,9 +7,16 @@ var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
 
 mongoose.connect('mongodb://localhost/commentDB',{ useNewUrlParser: true }); //Connects to a mongo database called "commentDB"
 
+var userSchema = mongoose.Schema({ //Defines the Schema for this database
+    Name: String,
+    Password: String
+});
+
+var User = mongoose.model('User', userSchema); //Makes an object from that schema as a model
+
 var commentSchema = mongoose.Schema({ //Defines the Schema for this database
     Name: String,
-    Comment: String
+    Password: String
 });
 
 var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
@@ -21,6 +28,68 @@ db.once('open', function() { //Lets us know when we're connected
 });
 
 
+
+
+router.post('/register', function(req, res, next) {
+  console.log("POST register route"); 
+  if(!req.body.Name || !req.body.Password){
+    res.json("empty")
+    return;
+  }
+  
+  User.find({ Name: req.body.Name }, function(err,results){
+    if (err) return console.error(err);//cthrow err;
+      if(results && results.length > 0)
+        res.json("username taken");
+      else{
+        res.json("registering user");
+        var newUser = new User(req.body);  
+        newUser.save(function(err, post) { 
+          if (err) return console.error(err);
+        });      
+      }
+  });//end find
+});
+
+
+
+router.post('/login', function(req, res, next) {
+  console.log("POST login route"); 
+  if(!req.body.Name || !req.body.Password){
+    res.json("empty")
+    return;
+  }
+  
+  User.find({ Name: req.body.Name }, function(err,results){
+    if (err) return console.error(err);
+      if(results && results.length > 0)
+        res.json("logged in");
+      else{
+        res.json("username doesn't exist");
+      }
+  });//end find
+});
+
+
+
+
+
+router.get('/users', function(req, res, next) {
+  console.log("GET user route");
+
+  User.find(function(err,userList) { //Calls the find() method on your database
+    if (err) return console.error(err); //If there's an error, print it out
+    else {
+    console.log(userList); //Otherwise console log the comments you found
+    res.json(userList); //Then send the comments
+  }
+})
+});
+
+router.post('/delete', function(req, res, next) {
+console.log("POST delete route");
+  User.remove().exec();
+});
 
 router.post('/comment', function(req, res, next) {
 console.log("POST comment route"); 
@@ -36,6 +105,19 @@ console.log(req.body);
     
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get('/comment', function(req, res, next) {
 console.log("In the GET route?");
 Comment.find(function(err,commentList) { //Calls the find() method on your database
@@ -48,12 +130,8 @@ Comment.find(function(err,commentList) { //Calls the find() method on your datab
 
 });
 
-router.post('/delete', function(req, res, next) {
-console.log("In the DELETE route?");
-  Comment.remove().exec();
-  
 
 
-});
+
 
 module.exports = router;
